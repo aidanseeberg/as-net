e94a10be1549b66f885eb9cf34949849e0045a31
Aidan Seeberg <aidan@Aidans-MacBook-Pro.local>
2024-12-15 11:42:01 -0600

    rm next_public

--- app/actions.ts
+++ app/actions.ts
@@ -20,7 +20,7 @@ interface TopAlbums {
 }
 export async function getAlbums(limit = 5) {
   const res: TopAlbums = await fetch(
-    `http://ws.audioscrobbler.com/2.0?method=user.getTopAlbums&period=7day&limit=${limit}&user=${process.env.NEXT_PUBLIC_LAST_USER}&format=json&api_key=${process.env.LAST_KEY}`
+    `http://ws.audioscrobbler.com/2.0?method=user.getTopAlbums&period=7day&limit=${limit}&user=${process.env.LAST_USER}&format=json&api_key=${process.env.LAST_KEY}`
   ).then((res) => res.json());
 
   return res.topalbums.album;
@@ -59,7 +59,7 @@ interface RecentTracks {
 }
 export async function getRecentTracks(limit = 5) {
   const res: RecentTracks = await fetch(
-    `http://ws.audioscrobbler.com/2.0?method=user.getRecentTracks&user=${process.env.NEXT_PUBLIC_LAST_USER}&limit=${limit}&page=1&format=json&api_key=${process.env.LAST_KEY}`
+    `http://ws.audioscrobbler.com/2.0?method=user.getRecentTracks&user=${process.env.LAST_USER}&limit=${limit}&page=1&format=json&api_key=${process.env.LAST_KEY}`
   ).then((res) => res.json());
 
   return res.recenttracks.track;
