8529de342e40aac13ee93665007d79cb7c6b0b8c
aidanseeberg <aidan.seeberg@gmail.com>
2025-03-02 13:17:08 -0600

    mvoed types folder outside app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cormorant } from "./fonts";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "aseebs.net",
   description: "aidan seeberg",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`bg-white text-black ${cormorant.className}`}>
